4ea440942640a59b8ef01cfcde787cc2fc03435c
scieslak <scieslak@scieslak.com>
2016-11-18 13:30:10 +0000

    Add name validations and tests for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+
+# Username validations
+VALID_USERNAME_REGEX = /\A\w+\z/i
+validates :name, presence: true,
+                     length: { in: 3..50 }
 end
--- test/models/user/name_user_test.rb
+++ test/models/user/name_user_test.rb
@@ -5,10 +5,22 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   def setup
     @user = users(:one)
+    @another_user = users(:two)
   end
 
-  test "user should not be valid" do
-    assert @user.valid?
+  test "name must be present" do
+    @user.name = "   "
+    assert_not @user.valid?
+  end
+
+  test "name should contain minimum 3 characters" do
+    @user.name = "uu"
+    assert_not @user.valid?
+  end
+
+  test "name should contain maximum 50 characters" do
+    @user.name = "u" * 51
+    assert_not @user.valid?
   end
 
 end
