979a40492f793cc115f99f336229e9d16d63993b
scieslak <scieslak@scieslak.com>
2016-11-21 21:27:25 +0000

    Add tests for user registraion

--- test/integration/user_registration_test.rb
+++ test/integration/user_registration_test.rb
@@ -2,5 +2,25 @@ require 'test_helper'
 
 class UserRegistrationTest < ActionDispatch::IntegrationTest
 
+  test "user should register providing valid information" do
+    visit new_user_path
+    fill_user_form(valid_user)
+    click_button "Register"
 
+  end
+
+  test "user should not register without valid information" do
+    visit new_user_path
+
+    fill_user_form(invalid_user)
+    click_button "Register"
+  end
+end
+
+
+def fill_user_form(user)
+  fill_in "Name",                  with: user.name
+  fill_in "Email",                 with: user.email
+  fill_in "Password",              with: user.password
+  fill_in "Password confirmation", with: user.password_confirmation
 end
