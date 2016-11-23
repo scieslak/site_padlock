9b844a954e0fe629b4d48e19450635652f42a342
scieslak <scieslak@scieslak.com>
2016-11-23 14:49:34 +0000

    Fix top bar partial

--- test/integration/user_show_test.rb
+++ test/integration/user_show_test.rb
@@ -9,11 +9,15 @@ class UserShowTest < ActionDispatch::IntegrationTest
 
   test "can view all users" do
    get users_path
+
+   # TODO: check for correct rendering user list if admin
   end
 
   test "user can view own profile" do
     get user_path(@user)
     assert_select "h1", @user.name
+
+    # TODO verify if current user
   end
 
 end
